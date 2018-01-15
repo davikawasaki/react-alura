dd998466d0e801d72945af2aaf34f999711d2ea2
Davi Kawasaki <davishinjik@gmail.com>
2018-01-15 19:56:13 -0200

    Error handler class with pubsub

---
+++ cdc-admin/src/ErrorHandler.js
@@ -0,0 +1,9 @@
+import PubSub from 'pubsub-js';
+
+export default class ErrorHandler {
+    publishErrors(obj) {
+        for(let i = 0; i < obj.errors.length; i++){
+            PubSub.publish("validation-error", obj.errors[i]);
+        }
+    }
+}
