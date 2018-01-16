ae8e8b95b4fdc2b89485d54f84b76b34b681edd4
Davi Kawasaki <davishinjik@gmail.com>
2018-01-16 00:06:15 -0200

    Router logic for switching components on main div

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import AuthorBox from './Author';
+import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    // <App />,
+    // React Router V3
+    // (<Router history={browserHistory}>
+    //     <Route path="/" component={App}>
+    //         <IndexRoute component={Home} />
+    //         <Route path="/author" component={AuthorBox} />
+    //         <Route path="/book" />
+    //     </Route>
+    // </Router>),
+    (<Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/author" component={AuthorBox} />
+                <Route path="/book" />
+            </Switch>
+        </App>
+    </Router>),
+    document.getElementById('root')
+);
 registerServiceWorker();
