0eaee638006f49091139317492056a159ff052e6
Davi Kawasaki <davishinjik@gmail.com>
2018-01-16 10:53:44 -0200

    Adding book component to route

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import AuthorBox from './Author';
+import BookBox from './Book';
 import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
@@ -21,7 +22,7 @@ ReactDOM.render(
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/author" component={AuthorBox} />
-                <Route path="/book" />
+                <Route path="/book" component={BookBox} />
             </Switch>
         </App>
     </Router>),
