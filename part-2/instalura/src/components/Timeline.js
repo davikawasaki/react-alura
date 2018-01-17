4b33a4455b9f8f6164f60a7d918fb2f2fd3f3488
Davi Kawasaki <davishinjik@gmail.com>
2018-01-16 23:17:15 -0200

    Receiving list photos dinamically and passing to inner components

--- part-2/instalura/src/components/Timeline.js
+++ part-2/instalura/src/components/Timeline.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import PhotoItem from './PhotoItem';
 
 export default class Timeline extends Component {
-    render(){
+
+    constructor() {
+        super();
+        this.state = {photosList: []};
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/public/fotos/alots')
+            .then(response => response.json())
+            .then(photos => this.setState({photosList: photos}));
+    }
+
+    render() {
         return (
         <div className="fotos container">
-            <PhotoItem/>
-            <PhotoItem/>
+            {
+                this.state.photosList.map(photo => <PhotoItem key={photo.id} photo={photo}/>)
+            }
         </div>            
         );
     }
