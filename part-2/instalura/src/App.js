3b78c5045f98fe929371bf734dc4d70521ef124c
Davi Kawasaki <davishinjik@gmail.com>
2018-01-18 14:14:10 -0200

    Changing styles from App to Index

--- part-2/instalura/src/App.js
+++ part-2/instalura/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import './css/reset.css';
-import './css/timeline.css';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
 
