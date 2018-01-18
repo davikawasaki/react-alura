554b196924bd5e1e40e01de5295bd4762c2aee88
Davi Kawasaki <davishinjik@gmail.com>
2018-01-18 18:27:12 -0200

    Loading public photos + checking params url change in component + storing login url in class

--- part-2/instalura/src/components/Timeline.js
+++ part-2/instalura/src/components/Timeline.js
@@ -3,15 +3,30 @@ import PhotoItem from './PhotoItem';
 
 export default class Timeline extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {       
+        super(props);        
         this.state = {photosList: []};
+        this.login = this.props.login;
+    }
+
+    loadPhotos() {
+        let urlProfile;       
+        if(this.login === undefined) urlProfile = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+        else urlProfile = `http://localhost:8080/api/public/fotos/${this.login}`;
+        fetch(urlProfile)
+            .then(response => response.json())
+            .then(photos => this.setState({photosList: photos}));
     }
 
     componentDidMount() {
-        fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
-            .then(response => response.json())
-            .then(photos => this.setState({photosList: photos}));
+        this.loadPhotos();
+    }
+
+    componentWillReceiveProps(nextProps) {          
+        if(nextProps.login !== undefined) {
+            this.login = nextProps.login;
+            this.loadPhotos();           
+        }
     }
 
     render() {
