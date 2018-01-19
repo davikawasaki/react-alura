50cfd85647f55d8dc81ba5134faa651e48fb6183
Davi Kawasaki <davishinjik@gmail.com>
2018-01-19 00:07:02 -0200

    Searching profile and publishing new photos to timeline with user login searched

--- part-2/instalura/src/components/Header.js
+++ part-2/instalura/src/components/Header.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
 
 export default class Header extends Component {
 
-    render() {
-        return (
-        <header className="header container">
-          <h1 className="header-logo">
-            Instalura
-          </h1>
+  search(event) {
+    event.preventDefault();
+    fetch(`http://localhost:8080/api/public/fotos/${this.loginSearch.value}`)
+      .then(response => response.json())
+      .then(photos => {
+        photos['login'] = this.loginSearch.value;
+        PubSub.publish('timeline-search', photos);
+      });
+  }
 
-          <form className="header-busca">
-            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo"/>
-            <input type="submit" value="Buscar" className="header-busca-submit"/>
-          </form>
+  render() {
+      return (
+      <header className="header container">
+        <h1 className="header-logo">
+          Instalura
+        </h1>
 
-          <nav>
-            <ul className="header-nav">
-              <li className="header-nav-item">
-                <a href="#">
-                  ♡
-                  {/*                 ♥ */}
-                  {/* Quem deu like nas minhas fotos */}
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>            
-        );
-    }
+        <form className="header-busca" onSubmit={this.search.bind(this)}>
+          <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo" ref={input => this.loginSearch = input}/>
+          <input type="submit" value="Buscar" className="header-busca-submit"/>
+        </form>
+
+        <nav>
+          <ul className="header-nav">
+            <li className="header-nav-item">
+              <a href="#">
+                ♡
+                {/*                 ♥ */}
+                {/* Quem deu like nas minhas fotos */}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>            
+      );
+  }
 }
