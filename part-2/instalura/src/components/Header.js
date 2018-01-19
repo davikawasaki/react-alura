d500e893e0ea5332a15f14682088e3182a68043b
Davi Kawasaki <davishinjik@gmail.com>
2018-01-19 11:33:16 -0200

    Adding logout link

--- part-2/instalura/src/components/Header.js
+++ part-2/instalura/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PubSub from 'pubsub-js';
 
 export default class Header extends Component {
@@ -28,11 +29,11 @@ export default class Header extends Component {
         <nav>
           <ul className="header-nav">
             <li className="header-nav-item">
-              <a href="#">
+              <Link to="/logout">
                 ♡
                 {/*                 ♥ */}
                 {/* Quem deu like nas minhas fotos */}
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
